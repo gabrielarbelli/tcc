d7def5a3424a194c3adeaf5838595901ac87e780
gabrielarbelli <gabriel_arbelli@hotmail.com>
2020-11-03 16:56:28 -0300

    Auto-generated commit

---
+++ assets/db.ts
@@ -0,0 +1,687 @@
+export default{
+    "data_covid_brasil": [
+        {
+            "country": "Brazil",
+            "cases": 726064,
+            "confirmed": 5409854,
+            "deaths": 157397,
+            "recovered": 4526393,
+            "updated_at": "2020-10-27T18:24:37.000Z"
+        }
+    ],
+    "data_covid_brasil_historico": [
+        {
+            "data": "20/10",
+            "deaths": 661
+        },
+        {
+            "data": "21/10",
+            "deaths": 566
+        },
+        {
+            "data": "22/10",
+            "deaths": 497
+        },
+        {
+            "data": "23/10",
+            "deaths": 571
+        },
+        {
+            "data": "24/10",
+            "deaths": 432
+        },
+        {
+            "data": "25/10",
+            "deaths": 231
+        },
+        {
+            "data": "26/10",
+            "deaths": 263
+        }
+    ],
+    "data_covid_estados": [
+        {
+            "uid": 35,
+            "uf": "SP",
+            "state": "São Paulo",
+            "cases": 1092843,
+            "deaths": 38753,
+            "suspects": 5334,
+            "refuses": 596,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 31,
+            "uf": "MG",
+            "state": "Minas Gerais",
+            "cases": 349512,
+            "deaths": 8779,
+            "suspects": 925,
+            "refuses": 104,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 29,
+            "uf": "BA",
+            "state": "Bahia",
+            "cases": 345949,
+            "deaths": 7497,
+            "suspects": 573,
+            "refuses": 36,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 33,
+            "uf": "RJ",
+            "state": "Rio de Janeiro",
+            "cases": 302746,
+            "deaths": 20213,
+            "suspects": 1254,
+            "refuses": 148,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 23,
+            "uf": "CE",
+            "state": "Ceará",
+            "cases": 270577,
+            "deaths": 9279,
+            "suspects": 493,
+            "refuses": 89,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 52,
+            "uf": "GO",
+            "state": "Goiás",
+            "cases": 248361,
+            "deaths": 5599,
+            "suspects": 353,
+            "refuses": 55,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 15,
+            "uf": "PA",
+            "state": "Pará",
+            "cases": 248024,
+            "deaths": 6725,
+            "suspects": 43,
+            "refuses": 23,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 42,
+            "uf": "SC",
+            "state": "Santa Catarina",
+            "cases": 247477,
+            "deaths": 3041,
+            "suspects": 346,
+            "refuses": 47,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 43,
+            "uf": "RS",
+            "state": "Rio Grande do Sul",
+            "cases": 235673,
+            "deaths": 5615,
+            "suspects": 416,
+            "refuses": 330,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 53,
+            "uf": "DF",
+            "state": "Distrito Federal",
+            "cases": 210145,
+            "deaths": 3641,
+            "suspects": 327,
+            "refuses": 107,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 41,
+            "uf": "PR",
+            "state": "Paraná",
+            "cases": 208251,
+            "deaths": 5107,
+            "suspects": 400,
+            "refuses": 119,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 21,
+            "uf": "MA",
+            "state": "Maranhão",
+            "cases": 184122,
+            "deaths": 4003,
+            "suspects": 125,
+            "refuses": 13,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 26,
+            "uf": "PE",
+            "state": "Pernambuco",
+            "cases": 159476,
+            "deaths": 8564,
+            "suspects": 89,
+            "refuses": 23,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 13,
+            "uf": "AM",
+            "state": "Amazonas",
+            "cases": 157668,
+            "deaths": 4460,
+            "suspects": 18,
+            "refuses": 26,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 32,
+            "uf": "ES",
+            "state": "Espírito Santo",
+            "cases": 151185,
+            "deaths": 3799,
+            "suspects": 71,
+            "refuses": 21,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 51,
+            "uf": "MT",
+            "state": "Mato Grosso",
+            "cases": 140273,
+            "deaths": 3749,
+            "suspects": 45,
+            "refuses": 8,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 25,
+            "uf": "PB",
+            "state": "Paraíba",
+            "cases": 130916,
+            "deaths": 3062,
+            "suspects": 85,
+            "refuses": 11,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 22,
+            "uf": "PI",
+            "state": "Piauí",
+            "cases": 110278,
+            "deaths": 2352,
+            "suspects": 12,
+            "refuses": 2,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 27,
+            "uf": "AL",
+            "state": "Alagoas",
+            "cases": 90254,
+            "deaths": 2216,
+            "suspects": 43,
+            "refuses": 22,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 28,
+            "uf": "SE",
+            "state": "Sergipe",
+            "cases": 83863,
+            "deaths": 2178,
+            "suspects": 15,
+            "refuses": 11,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 50,
+            "uf": "MS",
+            "state": "Mato Grosso do Sul",
+            "cases": 80064,
+            "deaths": 1555,
+            "suspects": 93,
+            "refuses": 26,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 24,
+            "uf": "RN",
+            "state": "Rio Grande do Norte",
+            "cases": 79908,
+            "deaths": 2563,
+            "suspects": 108,
+            "refuses": 22,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 17,
+            "uf": "TO",
+            "state": "Tocantins",
+            "cases": 74229,
+            "deaths": 1084,
+            "suspects": 13,
+            "refuses": 0,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 11,
+            "uf": "RO",
+            "state": "Rondônia",
+            "cases": 70600,
+            "deaths": 1442,
+            "suspects": 61,
+            "refuses": 2,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 14,
+            "uf": "RR",
+            "state": "Roraima",
+            "cases": 55951,
+            "deaths": 691,
+            "suspects": 9,
+            "refuses": 0,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 16,
+            "uf": "AP",
+            "state": "Amapá",
+            "cases": 51192,
+            "deaths": 741,
+            "suspects": 15,
+            "refuses": 0,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        },
+        {
+            "uid": 12,
+            "uf": "AC",
+            "state": "Acre",
+            "cases": 30317,
+            "deaths": 689,
+            "suspects": 12,
+            "refuses": 0,
+            "datetime": "2020-10-26T22:37:35.097Z"
+        }
+    ],
+    "data_doencas_graves": [
+        {
+            "ano": 2001,
+            "parasitarias": 45031,
+            "neoplasias": 125343,
+            "aparelho_circulatorio": 263414
+        },
+        {
+            "ano": 2002,
+            "parasitarias": 45174,
+            "neoplasias": 129921,
+            "aparelho_circulatorio": 267488
+        },
+        {
+            "ano": 2003,
+            "parasitarias": 46533,
+            "neoplasias": 134691,
+            "aparelho_circulatorio": 274068
+        },
+        {
+            "ano": 2004,
+            "parasitarias": 46067,
+            "neoplasias": 140801,
+            "aparelho_circulatorio": 285543
+        },
+        {
+            "ano": 2005,
+            "parasitarias": 46628,
+            "neoplasias": 147418,
+            "aparelho_circulatorio": 283927
+        },
+        {
+            "ano": 2006,
+            "parasitarias": 46508,
+            "neoplasias": 155796,
+            "aparelho_circulatorio": 302817
+        },
+        {
+            "ano": 2007,
+            "parasitarias": 45945,
+            "neoplasias": 161491,
+            "aparelho_circulatorio": 308466
+        },
+        {
+            "ano": 2008,
+            "parasitarias": 47295,
+            "neoplasias": 167677,
+            "aparelho_circulatorio": 317797
+        },
+        {
+            "ano": 2009,
+            "parasitarias": 47010,
+            "neoplasias": 172255,
+            "aparelho_circulatorio": 320074
+        },
+        {
+            "ano": 2010,
+            "parasitarias": 48823,
+            "neoplasias": 178990,
+            "aparelho_circulatorio": 326371
+        },
+        {
+            "ano": 2011,
+            "parasitarias": 49175,
+            "neoplasias": 184384,
+            "aparelho_circulatorio": 335213
+        },
+        {
+            "ano": 2012,
+            "parasitarias": 49608,
+            "neoplasias": 191577,
+            "aparelho_circulatorio": 333295
+        },
+        {
+            "ano": 2013,
+            "parasitarias": 52058,
+            "neoplasias": 196948,
+            "aparelho_circulatorio": 339669
+        },
+        {
+            "ano": 2014,
+            "parasitarias": 52174,
+            "neoplasias": 201968,
+            "aparelho_circulatorio": 340284
+        },
+        {
+            "ano": 2015,
+            "parasitarias": 55001,
+            "neoplasias": 208754,
+            "aparelho_circulatorio": 346685
+        },
+        {
+            "ano": 2016,
+            "parasitarias": 57188,
+            "neoplasias": 215217,
+            "aparelho_circulatorio": 362091
+        },
+        {
+            "ano": 2017,
+            "parasitarias": 57188,
+            "neoplasias": 221821,
+            "aparelho_circulatorio": 358882
+        }
+    ],
+    "data_doencas_parasitarias": [
+        {
+            "ano": 2001,
+            "aids": 10948,
+            "chagas": 4889,
+            "tuberculose": 5425,
+            "diarreia_e_gastroenterite": 5264
+        },
+        {
+            "ano": 2002,
+            "aids": 11055,
+            "chagas": 4891,
+            "tuberculose": 5162,
+            "diarreia_e_gastroenterite": 5069
+        },
+        {
+            "ano": 2003,
+            "aids": 11283,
+            "chagas": 5016,
+            "tuberculose": 4987,
+            "diarreia_e_gastroenterite": 5448
+        },
+        {
+            "ano": 2004,
+            "aids": 11020,
+            "chagas": 5041,
+            "tuberculose": 4981,
+            "diarreia_e_gastroenterite": 4895
+        },
+        {
+            "ano": 2005,
+            "aids": 11100,
+            "chagas": 4916,
+            "tuberculose": 4735,
+            "diarreia_e_gastroenterite": 5035
+        },
+        {
+            "ano": 2006,
+            "aids": 11046,
+            "chagas": 4930,
+            "tuberculose": 4823,
+            "diarreia_e_gastroenterite": 5364
+        },
+        {
+            "ano": 2007,
+            "aids": 11372,
+            "chagas": 4725,
+            "tuberculose": 4735,
+            "diarreia_e_gastroenterite": 4486
+        },
+        {
+            "ano": 2008,
+            "aids": 11839,
+            "chagas": 5096,
+            "tuberculose": 4881,
+            "diarreia_e_gastroenterite": 4512
+        },
+        {
+            "ano": 2009,
+            "aids": 12134,
+            "chagas": 4741,
+            "tuberculose": 4797,
+            "diarreia_e_gastroenterite": 4038
+        },
+        {
+            "ano": 2010,
+            "aids": 12151,
+            "chagas": 4876,
+            "tuberculose": 4659,
+            "diarreia_e_gastroenterite": 3999
+        },
+        {
+            "ano": 2011,
+            "aids": 12151,
+            "chagas": 4673,
+            "tuberculose": 4563,
+            "diarreia_e_gastroenterite": 3453
+        },
+        {
+            "ano": 2012,
+            "aids": 12073,
+            "chagas": 4650,
+            "tuberculose": 4421,
+            "diarreia_e_gastroenterite": 3652
+        },
+        {
+            "ano": 2013,
+            "aids": 12564,
+            "chagas": 4797,
+            "tuberculose": 4617,
+            "diarreia_e_gastroenterite": 4158
+        },
+        {
+            "ano": 2014,
+            "aids": 12575,
+            "chagas": 4428,
+            "tuberculose": 4467,
+            "diarreia_e_gastroenterite": 3667
+        },
+        {
+            "ano": 2015,
+            "aids": 12550,
+            "chagas": 4415,
+            "tuberculose": 4543,
+            "diarreia_e_gastroenterite": 3680
+        },
+        {
+            "ano": 2016,
+            "aids": 12540,
+            "chagas": 4461,
+            "tuberculose": 4483,
+            "diarreia_e_gastroenterite": 4075
+        },
+        {
+            "ano": 2017,
+            "aids": 11735,
+            "chagas": 4543,
+            "tuberculose": 4614,
+            "diarreia_e_gastroenterite": 4007
+        }
+    ],
+    "data_causas_externas": [
+        {
+            "ano": 2000,
+            "agressao": 45433,
+            "acidente_transporte": 28995,
+            "queda": 4258,
+            "lesao_involuntaria": 6780
+        },
+        {
+            "ano": 2001,
+            "agressao": 48032,
+            "acidente_transporte": 30524,
+            "queda": 5262,
+            "lesao_involuntaria": 7738
+        },
+        {
+            "ano": 2002,
+            "agressao": 49814,
+            "acidente_transporte": 32753,
+            "queda": 5273,
+            "lesao_involuntaria": 7724
+        },
+        {
+            "ano": 2003,
+            "agressao": 51534,
+            "acidente_transporte": 33139,
+            "queda": 6017,
+            "lesao_involuntaria": 7861
+        },
+        {
+            "ano": 2004,
+            "agressao": 48909,
+            "acidente_transporte": 35105,
+            "queda": 6617,
+            "lesao_involuntaria": 8017
+        },
+        {
+            "ano": 2005,
+            "agressao": 48136,
+            "acidente_transporte": 35994,
+            "queda": 6876,
+            "lesao_involuntaria": 8550
+        },
+        {
+            "ano": 2006,
+            "agressao": 49704,
+            "acidente_transporte": 36367,
+            "queda": 7903,
+            "lesao_involuntaria": 8639
+        },
+        {
+            "ano": 2007,
+            "agressao": 48219,
+            "acidente_transporte": 37407,
+            "queda": 8318,
+            "lesao_involuntaria": 8868
+        },
+        {
+            "ano": 2008,
+            "agressao": 50659,
+            "acidente_transporte": 38273,
+            "queda": 8801,
+            "lesao_involuntaria": 9328
+        },
+        {
+            "ano": 2009,
+            "agressao": 52043,
+            "acidente_transporte": 37594,
+            "queda": 9171,
+            "lesao_involuntaria": 9374
+        },
+        {
+            "ano": 2010,
+            "agressao": 53016,
+            "acidente_transporte": 42844,
+            "queda": 10426,
+            "lesao_involuntaria": 9448
+        },
+        {
+            "ano": 2011,
+            "agressao": 52807,
+            "acidente_transporte": 43256,
+            "queda": 10788,
+            "lesao_involuntaria": 9852
+        },
+        {
+            "ano": 2012,
+            "agressao": 56337,
+            "acidente_transporte": 44812,
+            "queda": 11649,
+            "lesao_involuntaria": 10321
+        },
+        {
+            "ano": 2013,
+            "agressao": 56803,
+            "acidente_transporte": 43452,
+            "queda": 12550,
+            "lesao_involuntaria": 10533
+        },
+        {
+            "ano": 2014,
+            "agressao": 59681,
+            "acidente_transporte": 43780,
+            "queda": 13327,
+            "lesao_involuntaria": 10653
+        },
+        {
+            "ano": 2015,
+            "agressao": 56212,
+            "acidente_transporte": 37306,
+            "queda": 12395,
+            "lesao_involuntaria": 10542
+        },
+        {
+            "ano": 2016,
+            "agressao": 61143,
+            "acidente_transporte": 37345,
+            "queda": 14832,
+            "lesao_involuntaria": 11433
+        },
+        {
+            "ano": 2017,
+            "agressao": 63748,
+            "acidente_transporte": 35375,
+            "queda": 15667,
+            "lesao_involuntaria": 12495
+        }
+    ],
+    "data_pandemias_mundo": [
+        {
+            "covid": 1161422,
+            "gripe_espanhola": 50000000,
+            "gripe_hong_kong": 3000000,
+            "gripe_asiatica": 2000000,
+            "gripe_suina": 17000
+        }
+    ],
+    "data_tragedias_mundo": [
+        {
+            "covid": 1161422,
+            "primeira_guerra": 20000000,
+            "segunda_guerra": 70000000,
+            "bomba_hiroshima": 140000,
+            "bomba_nagasaki": 74000,
+            "tsunami_japao_2011": 18428,
+            "acidente_nuclear_fukushima": 1600
+        }
+    ]
+}
